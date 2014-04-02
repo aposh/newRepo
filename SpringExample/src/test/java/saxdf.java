856e1bbc8cac98cea2e0354f63a6ed440eac327b
aposh <krznadolnik@gmail.com>
2014-04-02 22:20:00 +0200

    asdf

---
+++ SpringExample/src/test/java/saxdf.java
@@ -0,0 +1,22 @@
+
+public class saxdf {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		/*
+		 * 
+		 * 
+		 * 
+		 * 
+		 * 
+		 */
+		
+		
+	}
+
+}
